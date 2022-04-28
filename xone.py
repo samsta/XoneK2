a552306d6b85cb2ade9c3e1705a130b4253f4008
Sam Nobs <3915450+samsta@users.noreply.github.com>
2022-04-28 23:18:08 +1200

    Further improvements
    - Fix bug whereby adding or deleting an EqThree doesn't work properly
    - Add shift button to multiplex bottom encoders between tempo/scene selection and cue/master volume
    - Add clip start buttons and track stop buttons that reflect play status of selected clip and current track
    - Remove some unused cruft

--- xone.py
+++ xone.py
@@ -19,6 +19,8 @@ from _Framework.TransportComponent import TransportComponent
 g_logger = None
 DEBUG = True
 
+shift_is_pressed = False
+
 
 def log(msg):
     global g_logger
@@ -28,9 +30,6 @@ def log(msg):
 
 
 EQ_DEVICES = {
-    'Eq8': {
-        'Gains': ['%i Gain A' % (index + 1) for index in range(8)]
-    },
     'FilterEQ3': {
         'Gains': ['GainLo', 'GainMid', 'GainHi'],
         'Cuts': ['LowOn', 'MidOn', 'HighOn']
@@ -66,9 +65,28 @@ PUSH_ENCODER_LR = 14
 BUTTON_LL = 12
 BUTTON_LR = 15
 
+class Color():
+    RED = 0
+    YELLOW = 36
+    GREEN = 72
+
+class ClipState():
+    STOPPED = 0
+    TRIGGERED = 1
+    PLAYING = 2
+
+
+class ButtonWithLight(ButtonElement):
+    def __init__(self, notenr):
+        super(ButtonWithLight, self).__init__(True, MIDI_NOTE_TYPE, CHANNEL, notenr)
+        self.notenr = notenr
+
+    def release_parameter(self):
+        super(ButtonWithLight, self).release_parameter()
+        self.send_midi((144 + CHANNEL, self.notenr, 0)) # for some reason send_value() doesn't work reliably, so send raw MIDI
 
 def button(notenr, name=None):
-    rv = ButtonElement(True, MIDI_NOTE_TYPE, CHANNEL, notenr)
+    rv = ButtonWithLight(notenr)
     if name is not None:
         rv.name = name
     return rv
@@ -80,6 +98,7 @@ class EqGainEncoder(EncoderElement):
     def __init__(self, cc):
         super(EqGainEncoder,self).__init__(MIDI_CC_TYPE, CHANNEL, cc, Live.MidiMap.MapMode.absolute)
         self.add_value_listener(self.handle_encoder_turn)
+        self.mapped_param = None
 
     def handle_encoder_turn(self, value):
         zero_db = 0.85
@@ -94,12 +113,11 @@ class EqGainEncoder(EncoderElement):
             fval = zero_db
 
         if (self.mapped_parameter() != None):
-            self._mapped_param = self.mapped_parameter()
+            self.mapped_param = self.mapped_parameter()
             self.release_parameter()
 
-        if self._mapped_param != None:
-            self._mapped_param.value = fval
-            log("turn %d, mapped %f" % (value, self._mapped_param.value))
+        if self.mapped_param != None:
+            self.mapped_param.value = fval
 
 class Fader(SliderElement):
     def __init__(self, notenr, max=1.0):
@@ -131,8 +149,9 @@ class DynamicEncoder(EncoderElement):
         """
         self.growth = growth
         self.timeout = timeout
-        self.encoder = encoder(cc)
-        self.encoder.add_value_listener(self.handle_encoder_turn)
+        if cc != None:
+            self.encoder = encoder(cc)
+            self.encoder.add_value_listener(self.handle_encoder_turn)
         self.sensitivity = 1.0
         self.last_event_value = None
         self.last_event_time = 0
@@ -167,6 +186,7 @@ class TempoEncoder(EncoderElement):
         self.growth = growth
         self.timeout = timeout
         self.encoder = encoder(cc)
+        self.alternate_encoder = DynamicEncoder(None, transport.song().master_track.mixer_device.cue_volume)
         self.button = button(button_cc)
         self.encoder.add_value_listener(self.handle_encoder_turn)
         self.button.add_value_listener(self.handle_button)
@@ -175,7 +195,7 @@ class TempoEncoder(EncoderElement):
         self.last_event_time = 0
         self.transport = transport
         self.max_sensitivity = max_sensitivity
-        self.button_pressed = True
+        self.button_pressed = False
 
     def handle_button(self, value):
         self.button_pressed = value >= 64
@@ -188,6 +208,11 @@ class TempoEncoder(EncoderElement):
             self.transport.song().nudge_down = False
 
     def handle_encoder_turn(self, value):
+        global shift_is_pressed
+        if shift_is_pressed:
+            self.alternate_encoder.handle_encoder_turn(value)
+            return
+
         now = time.time()
 
         # nudge if button pressed, otherwise adjust tempo
@@ -213,25 +238,24 @@ class TempoEncoder(EncoderElement):
         self.last_event_time = now
         self.last_event_value = value
 
-
-
 class SceneSelector(EncoderElement):
-    def __init__(self, cc, button_cc, stop_button_cc, song):
+    def __init__(self, cc, button_cc, song):
         self.encoder = encoder(cc)
+        self.alternate_encoder = DynamicEncoder(None, song.master_track.mixer_device.volume)
         self.button = button(button_cc)
-        self.stop_button = button(stop_button_cc)
         self.encoder.add_value_listener(self.handle_encoder_turn)
         self.button.add_value_listener(self.handle_button)
-        self.stop_button.add_value_listener(self.handle_stop_button)
         self.song = song
 
     def handle_button(self, value):
         self.song.view.selected_scene.fire()
 
-    def handle_stop_button(self, value):
-        self.song.stop_all_clips()
-
     def handle_encoder_turn(self, value):
+        global shift_is_pressed
+        if shift_is_pressed:
+            self.alternate_encoder.handle_encoder_turn(value)
+            return
+
         scene_index = list(self.song.scenes).index(self.song.view.selected_scene)
         if value < 64:
             scene_index += 1
@@ -241,6 +265,32 @@ class SceneSelector(EncoderElement):
         if scene_index in range(len(self.song.scenes)):
             self.song.view.selected_scene = self.song.scenes[scene_index]
         
+class GlobalStopButton(ButtonElement):
+    def __init__(self, button_cc, song):
+        self.button = button(button_cc)
+        self.button.add_value_listener(self.handle_button)
+        self.song = song
+        song.add_is_playing_listener(self.handle_song_is_playing)
+        song.add_metronome_listener(self.handle_metronome)
+        self.last_stop_button_time = 0
+        log("song %s" % dir(song))
+
+    def handle_metronome(self):
+        log("metro")
+
+    def handle_song_is_playing(self):
+        self.button.send_value(127 if self.song.is_playing else 0)
+
+    def handle_button(self, value):
+        if value == 0:
+            return
+
+        now = time.time()
+        if now - self.last_stop_button_time < 0.4:
+            self.song.is_playing = False
+        self.song.stop_all_clips()
+        
+        self.last_stop_button_time = now
 
 class MixerWithDevices(MixerComponent):
     def __init__(self, num_tracks, num_returns=0, device_select=None, device_encoders=None):
@@ -290,7 +340,6 @@ class MixerWithDevices(MixerComponent):
 
     def attach_encoders(self):
         for control, target in zip(self.encoders, self.devices[self.active_track]["params"]):
-            log("c %s t %s" % (control, target))
             control.target = target
 
     def get_active_tracks(self):
@@ -386,29 +435,21 @@ class MixerWithDevices(MixerComponent):
         if track is not None:
             # listen for changes to the device chain
             def dcb():
-                return self._on_eq_changed(i)
+                return self._reassign_tracks()
             dev["cb"] = dcb
             dev["track"] = track
             track.add_devices_listener(dcb)
-
-            # force an update to attach to any existing device
-            dcb()
-
-    def _on_eq_changed(self, i):
-        log("_on_eq_changed %d" % i)
-        # the device chain on track i changed-- reassign device if needed
-        track = self.eqs[i]["track"]
-        device_comp = self.eqs[i]["component"]
-        device = None
-        if track.devices:
-            # Find the first EQ device.
-            for dev in track.devices:
-                log("examine device %s" % dev.class_name)
-                if dev.class_name in EQ_DEVICES:
-                    log("using it")
-                    device = dev
-                    break
-        device_comp.set_lock_to_device(True, device)
+            device_comp = self.eqs[i]["component"]
+            device = None
+            if track.devices:
+                # Find the first EQ device.
+                for dev in track.devices:
+                    log("examine device %s" % dev.class_name)
+                    if dev.class_name in EQ_DEVICES:
+                        log("using it")
+                        device = dev
+                        break
+            device_comp.set_lock_to_device(True, device)
         self.update()
 
     def set_eq_controls(self, track_nr, controls):
@@ -422,27 +463,83 @@ class MixerWithDevices(MixerComponent):
         device_comp.update()
 
 
+class ClipStartButton(ButtonElement):
+    def __init__(self, tracknr, notenr, song):
+        super(ClipStartButton, self).__init__(True, MIDI_NOTE_TYPE, CHANNEL, notenr)
+        self.song = song
+        self.tracknr = tracknr
+        self.notenr = notenr
+        self.add_value_listener(self.push_button)
+
+    def set_clip_selected(self, is_selected, state):
+        if is_selected:
+            if state == ClipState.PLAYING:
+                color = Color.RED
+            elif state == ClipState.TRIGGERED:
+                color = Color.YELLOW
+            else:
+                color = Color.GREEN
+
+            self.send_midi((144 + CHANNEL, self.notenr + color, 127))    
+        else:
+            self.send_midi((144 + CHANNEL, self.notenr, 0))    
+
+    def push_button(self, value):
+        if value > 1:
+            self.song.view.selected_scene.clip_slots[self.tracknr].fire()
+
+class TrackStopButton(ButtonElement):
+    def __init__(self, tracknr, notenr, mixer):
+        super(TrackStopButton, self).__init__(True, MIDI_NOTE_TYPE, CHANNEL, notenr)
+        self.mixer = mixer
+        self.tracknr = tracknr
+        self.notenr = notenr
+        self.add_value_listener(self.push_button)
+
+    def set_track_playing(self, is_playing):
+        if is_playing:
+            self.send_midi((144 + CHANNEL, self.notenr, 127))    
+        else:
+            self.send_midi((144 + CHANNEL, self.notenr, 0))    
+
+    def push_button(self, value):
+        if value > 1:
+            self.mixer.channel_strip(self.tracknr)._track.stop_all_clips()
+
 class XoneK2_DJ(ControlSurface):
     def __init__(self, instance):
         global g_logger
         g_logger = self.log_message
         super(XoneK2_DJ, self).__init__(instance, False)
+        self.tracks_with_listener = []
+        self.slots_with_listener = []
+        self.clips_with_listener = []
+        self.slot_callbacks = []
+        self.clip_callbacks = []
+
         with self.component_guard():
             self._set_suppress_rebuild_requests(True)
             self.init_session()
             self.init_mixer()
-            #self.init_matrix()
             self.init_tempo()
 
-            # connect mixer to session
-            #self.session.set_mixer(self.mixer)
-            #self.session.update()
-            #self.set_highlighting_session_component(self.session)
+            self.shift_button = button(BUTTON_LL)
+            self.shift_button.add_value_listener(self.press_shift)
+
+            self.remove_clip_listeners()
+            self.add_clip_listeners()
+            self.update_track_playing_status()
+
             self._set_suppress_rebuild_requests(False)
 
-    def init_session(self):
-        self.scene_selector = SceneSelector(ENCODER_LR, PUSH_ENCODER_LR, BUTTON_LR, self.song())
+    def press_shift(self, value):
+        global shift_is_pressed
+        self.shift_button.send_value(value)
+        shift_is_pressed = value > 0
 
+    def init_session(self):
+        self.scene_selector = SceneSelector(ENCODER_LR, PUSH_ENCODER_LR, self.song())
+        self.global_stop_button = GlobalStopButton(BUTTON_LR, self.song())
 
     def init_mixer(self):
         self.mixer = MixerWithDevices(
@@ -455,9 +552,11 @@ class XoneK2_DJ(ControlSurface):
         self.song().view.selected_track = self.mixer.channel_strip(0)._track
 
         volume_limit = 0.85 # 0dB
+        self.clip_start_buttons = []
+        self.track_stop_buttons = []
         for i in range(NUM_TRACKS):
             self.mixer.channel_strip(i).set_volume_control(Fader(FADERS[i], volume_limit))
-            #self.mixer.channel_strip(i).set_solo_button(button(BUTTONS3[i]))
+            self.mixer.channel_strip(i).set_solo_button(button(GRID[2][i]))
             self.mixer.set_eq_controls(i, (
                 EqGainEncoder(KNOBS3[i]),
                 EqGainEncoder(KNOBS2[i]),
@@ -466,34 +565,113 @@ class XoneK2_DJ(ControlSurface):
                 button(BUTTONS3[i]),
                 button(BUTTONS2[i]),
                 button(BUTTONS1[i])))
-            #self.mixer.set_device_controls(i, button(BUTTONS1[i]))
+            self.clip_start_buttons.append(ClipStartButton(i, GRID[0][i], self.song()))
+            self.track_stop_buttons.append(TrackStopButton(i, GRID[1][i], self.mixer))
+            self.mixer.channel_strip(i)._track.add_clip_slots_listener(self.on_track_changed)
 
-        #self.master_encoder = DynamicEncoder(
-        #    ENCODER_LR, self.song().master_track.mixer_device.volume)
-        #self.cue_encoder = DynamicEncoder(
-        #    ENCODER_LL, self.song().master_track.mixer_device.cue_volume)
+        self.song().view.add_selected_scene_listener(self.on_scene_changed)
         self.mixer.update()
 
-    def init_matrix(self):
-        self.matrix = ButtonMatrixElement()
+    def on_scene_changed(self):
+        log("scene changed")
+        self.update_track_playing_status()
 
-        for scene_index in range(NUM_SCENES):
-            scene = self.session.scene(scene_index) 
-            scene.name = 'Scene ' + str(scene_index)
-            button_row = []
-            for track_index in range(NUM_TRACKS):
-                note_nr = GRID[scene_index][track_index]
-                b = button(note_nr, name='Clip %d, %d button' % (scene_index, track_index))
-                button_row.append(b)
-                clip_slot = scene.clip_slot(track_index)
-                clip_slot.name = 'Clip slot %d, %d' % (scene_index, track_index)
-                clip_slot.set_stopped_value(0)
-                clip_slot.set_started_value(64)
-                clip_slot.set_launch_button(b)
-            self.matrix.add_row(tuple(button_row))
-        #stop_buttons = [button(note_nr) for note_nr in BUTTONS2]
-        #self.session.set_stop_track_clip_buttons(stop_buttons)
+    def on_track_changed(self):
+        log("track changed")
+        self.remove_clip_listeners()
+        self.add_clip_listeners()
+        self.update_track_playing_status()
 
     def init_tempo(self):
         self.transport = TransportComponent()
         self.tempo = TempoEncoder(ENCODER_LL, PUSH_ENCODER_LL, self.transport)
+
+    def on_slot_clip_changed(self, slot, track_idx, slot_idx):
+        self.remove_clip_listeners()
+        self.add_clip_listeners()
+        self.update_track_playing_status()
+
+    def on_clip_playing_changed(self, clip, track_idx, clip_idx):
+        self.update_track_playing_status()
+
+    def is_track_playing(self, track):
+        for slot in track.clip_slots:
+            if slot.has_clip and slot.clip.is_playing:
+                return 1
+
+        return 0
+
+    def is_selected_slot_playing(self, track_idx):
+        slot = self.song().view.selected_scene.clip_slots[track_idx]
+        state = ClipState.STOPPED
+        if slot.has_clip:
+            if slot.clip.is_playing:
+                state = ClipState.PLAYING
+            elif slot.clip.is_triggered:
+                state = ClipState.TRIGGERED
+        return slot.has_clip, state
+
+
+    def update_track_playing_status(self):
+        i = 0
+        for track in self.song().tracks:
+            if i >= NUM_TRACKS:
+                return
+
+            is_selected, clip_state = self.is_selected_slot_playing(i)
+            self.clip_start_buttons[i].set_clip_selected(is_selected, clip_state)
+            self.track_stop_buttons[i].set_track_playing(self.is_track_playing(track))
+            i += 1
+
+    def clip_add_callback(self, clip, track_idx, clip_idx):
+        callback = lambda : self.on_clip_playing_changed(clip, track_idx, clip_idx)
+        clip.add_playing_status_listener(callback)
+        self.clips_with_listener += [clip]
+        self.clip_callbacks += [callback]
+
+    def slot_add_callback(self, slot, track_idx, slot_idx):
+        callback = lambda : self.on_slot_clip_changed(slot, track_idx, slot_idx)
+        slot.add_has_clip_listener(callback)
+        self.slots_with_listener += [slot]
+        self.slot_callbacks += [callback]
+
+    def add_clip_listeners(self):
+        i = 0
+        for track in self.song().tracks:
+            if i >= NUM_TRACKS:
+                return
+            sloti = 0
+            for slot in track.clip_slots:
+                if slot.has_clip:
+                    self.clip_add_callback(slot.clip, i, sloti)
+                self.slot_add_callback(slot, i, sloti)
+                sloti += 1
+
+            i += 1
+
+    def remove_slot_listeners(self):
+        for i in range(0, len(self.slots_with_listener)):
+            slot = self.slots_with_listener[i]
+            callback = self.slot_callbacks[i]
+            try:
+                if slot.has_clip_has_listener(callback):
+                    slot.remove_has_clip_listener(callback)
+            except:
+                continue
+
+        self.slots_with_listener = []
+        self.slot_callbacks = []
+
+    def remove_clip_listeners(self):
+        self.remove_slot_listeners()
+        for i in range(0, len(self.clips_with_listener)):
+            clip = self.clips_with_listener[i]
+            callback = self.clip_callbacks[i]
+            try:
+                if clip.playing_status_has_listener(callback):
+                    clip.remove_playing_status_listener(callback)
+            except:
+                continue
+
+        self.clips_with_listener = []
+        self.clip_callbacks = []
